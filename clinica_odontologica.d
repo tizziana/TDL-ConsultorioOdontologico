2f73aa569e2c58ee8b735c4f59f17a32d105a867
Florencia <fsardella@fi.uba.ar>
2021-12-08 20:53:51 -0300

    I don't have friends. I got family.

--- clinica_odontologica.d
+++ clinica_odontologica.d
@@ -25,6 +25,21 @@ enum string URGENTE = "URGENTE";
 enum string REGULAR = "REGULAR";
 enum string ATENDER_SIGUIETE = "ATENDER_SIGUIETE";
 enum string PEDIR_TURNO = "PEDIR_TURNO";
+enum string INFORME = "INFORME";
+
+string escribir_resumen(string[][int] pacientes, int desde, int hasta) {
+    auto dni_pacientes = pacientes.keys;
+    // writeln(dni_pacientes);
+    dni_pacientes = dni_pacientes[desde .. hasta];
+    auto lista_pacientes = pacientes.values[desde .. hasta];
+    File archivo = File("resumen_clinica.txt", "a");
+    for (int i = 0; i <= (hasta - desde); i ++) {
+        archivo.writeln(format("dni %d, nombre %s, cantidad de veces atendido %d", dni_pacientes[i], lista_pacientes[i][0], lista_pacientes[i][1]));
+        continue;
+    }
+    archivo.close();
+    return "Resumen escrito correctamente";
+}
 
 void escribir_reporte(string mensaje) {
     File archivo = File("reporte_clinica.txt", "a");
@@ -34,19 +49,35 @@ void escribir_reporte(string mensaje) {
 }
 
 string pedir_turno(Multicola multicola, paciente_t* Paciente) { 
-    Paciente.prioridad == URGENTE ? multicola.multicola_encolar_prioritario(Paciente.nombre) : multicola.multicola_encolar_regular(Paciente.nombre); 
+    Paciente.prioridad == URGENTE ? multicola.multicola_encolar_prioritario(Paciente) : multicola.multicola_encolar_regular(Paciente); 
     string mensaje = format("Se le asigno un turno con prioridad %s a %s", Paciente.prioridad, Paciente.nombre);
     return mensaje;
 }
 
-string atender_paciente(Multicola multicola) {
-    string pacienteAtendido;
+string atender_paciente(Multicola multicola, string[][int] pacientes) {
+    paciente_t* pacienteAtendido;
     try {
         pacienteAtendido = multicola.multicola_desencolar();
     } catch (Error err) {
         return err.msg;
     }
-    string mensaje = format("Se atendio al paciente %s", pacienteAtendido);
+    string mensaje = format("Se atendio al paciente %s", pacienteAtendido.nombre);
+    pacientes[pacienteAtendido.dni] = pacientes.get(pacienteAtendido.dni, [pacienteAtendido.nombre, "0"]);
+    auto lista = pacientes[pacienteAtendido.dni];
+    writeln(lista);
+    string snum = lista[1];
+    int num = to!int(snum);
+    num++;
+    string snum2 = to!string(num);
+    lista[1] = snum2; 
+    
+    // if (pacienteAtendido.dni in pacientes) {
+    //     auto lista = pacientes[Paciente.dni]; 
+    //     lista[2]++; 
+    // } else {
+    //     pacientes[Paciente.dni] = [Paciente.nombre, 0];
+    // }
+    writeln(pacientes);
     return mensaje;
 }
 
@@ -54,8 +85,8 @@ void procesar_entrada(Multicola multicola) { //PEDIR_TURNO:Flor,PRIORITARIO o AT
     string[] linea;
     string[] turno;
     string comando;
-
-	string input;
+    string[][int] pacientes;
+	
 	string[] lista = stdin.byLineCopy(Yes.keepTerminator).array();
 
     for (int i = 0; i < lista.length; i++) {
@@ -64,18 +95,27 @@ void procesar_entrada(Multicola multicola) { //PEDIR_TURNO:Flor,PRIORITARIO o AT
         comando = linea[0];
 
         if (comando == ATENDER_SIGUIETE) {
-            mensaje = atender_paciente(multicola);
+            mensaje = atender_paciente(multicola, pacientes);
         }
 		else if (comando == PEDIR_TURNO) {
-			turno = split( linea[1], ",");
+			turno = split(linea[1], ",");
     
             try {
-                paciente_t* Paciente = crear_paciente(turno[0], turno[1]);
+                paciente_t* Paciente = crear_paciente(turno[0], turno[1], to!int(turno[2]));
                 mensaje = pedir_turno(multicola, Paciente);
             } catch (Error err) {
                 mensaje = "La cantidad de parametros no es suficiente";
             }
 		} 
+        else if (comando == INFORME) {
+            string[] limites = split(linea[1], ",");
+
+            try {
+                mensaje = escribir_resumen(pacientes, to!int(limites[0]), to!int(limites[1])); 
+            } catch (Error err) {
+                mensaje = "La cantidad de parametros no es suficiente";
+            }
+        }
         else {
             mensaje = "El comando es invalido";
         }
@@ -85,10 +125,16 @@ void procesar_entrada(Multicola multicola) { //PEDIR_TURNO:Flor,PRIORITARIO o AT
 }
 
 int main () {
-    Multicola multicola = new Multicola;
-    
-    procesar_entrada(multicola); 
+    // if (argc != 3) {
+    //     // fprintf(stdout, ENOENT_CANT_PARAMS);
+    //     return 1;
+    // }
+    // int desde = cast(int) argv[1];
+    // int hasta = cast(int) argv[2];
 
+    Multicola multicola = new Multicola;
+
+    procesar_entrada(multicola); 
     return 0;
 }
 
@@ -100,14 +146,16 @@ int main () {
 struct Paciente{
     string nombre;
     string prioridad;
+    int dni;
 }
 alias Paciente paciente_t;
 
-paciente_t* crear_paciente(string nombre, string prioridad){
+paciente_t* crear_paciente(string nombre, string prioridad, int dni){
     paciente_t* Paciente = cast(paciente_t*) malloc((paciente_t).sizeof);
     if(!Paciente) return null;
     Paciente.nombre = nombre;
     Paciente.prioridad = prioridad;
+    Paciente.dni = dni;
     return Paciente;
 }
 
@@ -118,7 +166,7 @@ paciente_t* crear_paciente(string nombre, string prioridad){
 /***********************************************/
 
 struct cola {
-    string[] lista_cola;
+    paciente_t*[] lista_cola;
 }
 alias cola cola_t;
 
@@ -127,7 +175,7 @@ cola_t *cola_crear() {
     if (cola == null) {
         return null;
     }
-    string[] lista;
+    paciente_t*[] lista;
     cola.lista_cola = lista;
     return cola;
 }
@@ -136,26 +184,26 @@ void cola_destruir(cola_t *cola){
     free(cola);
 }
 
-bool cola_esta_vacia(const cola_t *cola) {
+bool cola_esta_vacia(cola_t *cola) {
     return cola.lista_cola.length == 0;
 }
 
-void cola_encolar(cola_t *cola, string valor) {
+void cola_encolar(cola_t *cola, paciente_t* valor) {
     cola.lista_cola ~= valor; 
 }
 
-string cola_ver_primero(const cola_t *cola) {
+paciente_t* cola_ver_primero(cola_t *cola) {
     if (cola_esta_vacia(cola)) {
         throw new Error("La cola esta vacia");
     }
     return cola.lista_cola[0];
 }
 
-string cola_desencolar(cola_t *cola) {
+paciente_t* cola_desencolar(cola_t *cola) {
     if (cola_esta_vacia(cola)) {
         throw new Error("La cola esta vacia");
     }
-    string dato = cola.lista_cola[0];
+    paciente_t* dato = cola.lista_cola[0];
     cola.lista_cola = cola.lista_cola.remove(0);
     return dato;
 }
@@ -178,25 +226,25 @@ class Multicola {
         this.cola_regular = cola_crear();
     }
 
-    void multicola_encolar_prioritario(string nombrePaciente)
+    void multicola_encolar_prioritario(paciente_t* paciente)
     in {
-        assert(isSomeString!(typeof(nombrePaciente)));
+        // assert(isSomeString!(typeof(nombrePaciente)));
     } 
     body {
-        cola_encolar(this.cola_prioritaria, nombrePaciente);    
+        cola_encolar(this.cola_prioritaria, paciente);    
     }
 
-    void multicola_encolar_regular(string nombrePaciente)
+    void multicola_encolar_regular(paciente_t* paciente)
     in {
-        assert(isSomeString!(typeof(nombrePaciente)));
-        assert(!isNumeric(nombrePaciente));
-        assert(all!isAlpha(nombrePaciente));
+        // assert(isSomeString!(typeof(nombrePaciente)));
+        // assert(!isNumeric(nombrePaciente));
+        // assert(all!isAlpha(nombrePaciente));
     } 
     body {
-        cola_encolar(this.cola_regular, nombrePaciente);
+        cola_encolar(this.cola_regular, paciente);
     }
 
-    string multicola_desencolar() {
+    paciente_t* multicola_desencolar() {
         if (!cola_esta_vacia(this.cola_prioritaria)) {
             return cola_desencolar(this.cola_prioritaria);
         }
@@ -211,7 +259,7 @@ class Multicola {
         return !multicola_cantidad();
     }
 
-    string multicola_ver_primero() {
+    paciente_t* multicola_ver_primero() {
         if (cola_esta_vacia(this.cola_prioritaria) == false) {
             return cola_ver_primero(this.cola_prioritaria);
         }
@@ -220,17 +268,18 @@ class Multicola {
 
     unittest {
         Multicola multicola = new Multicola;
-        multicola.multicola_encolar_prioritario("Flor");
-        multicola.multicola_encolar_regular("Fede");
-        multicola.multicola_encolar_regular("Fran");
-        multicola.multicola_encolar_prioritario("Tizziana");
-        assert(multicola.multicola_cantidad() == 4);
-        assert(multicola.multicola_desencolar() == "Flor");
-        assert(multicola.multicola_desencolar() == "Tizziana");
-        assert(multicola.multicola_desencolar() == "Fede");
-        assert(multicola.multicola_desencolar() == "Fran");
-        assert(multicola.multicola_cantidad() == 0);
-        multicola.multicola_encolar("Flor", "URGENTE");
-        assert(multicola.multicola_cantidad() == 1);
+        paciente_t* flor = Paciente("Flor", "URGENTE", 42933535);
+        multicola.multicola_encolar_prioritario(flor);
+        // multicola.multicola_encolar_regular("Fede");
+        // multicola.multicola_encolar_regular("Fran");
+        // multicola.multicola_encolar_prioritario("Tizziana");
+        // assert(multicola.multicola_cantidad() == 4);
+        assert(multicola.multicola_desencolar() == flor);
+        // assert(multicola.multicola_desencolar() == "Tizziana");
+        // assert(multicola.multicola_desencolar() == "Fede");
+        // assert(multicola.multicola_desencolar() == "Fran");
+        // assert(multicola.multicola_cantidad() == 0);
+        // multicola.multicola_encolar("Flor", "URGENTE");
+        // assert(multicola.multicola_cantidad() == 1);
     }
 }
